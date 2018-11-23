7bdb1e9bf702cc927703e0f8c005c61b3048b34e
Natasha S Love <debora@inbox.ru>
2018-11-22 20:28:32 -0600

    Update Program.cs

--- nLove_homework05/Program.cs
+++ nLove_homework05/Program.cs
@@ -8,28 +8,40 @@ namespace nLove_homework05
         {
 
             string [] names = new string [5] { "Ann", "Betty", "Clair", "Donna", "Helen" };
+            int number;
+            string answer = "yes";
 
-            Console.WriteLine(names[0]);
-            /*
-             * try
+            //Console.WriteLine(names[0]);
+
+            while (answer == "yes")
+            {
+
+                try
                 {
-                    Console.WriteLine("How many miles did you go?");
-                    t.Miles = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("How many gallons did you use?");
-                    t.Gallons = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("You got about {0} miles per gallon.", t.MPG);
+                    Console.WriteLine("Hello, please type an integer:");
+                    string input = Console.ReadLine();
+
+                    // TryParse returns true if the conversion succeeded
+                    // and stores the result in number.
+
+                    while (!Int32.TryParse(input, out number))
+                    {
+
+                        Console.WriteLine("String could not be parsed.");
+                        Console.WriteLine("Please type an integer:");
+                        input = Console.ReadLine();
+                    }
+                    Console.WriteLine(names[number]);
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine("Error!");
+                    Console.WriteLine(ex.Message);
                 }
 
-                catch (NegativeMilesException ex)
-                {
-
-                    Console.WriteLine(ex.Message);
-                } 
-             * 
-             */
-
-
-
+                Console.WriteLine("Try again? (yes/no)");
+                answer = Console.ReadLine();
+            }
             Console.WriteLine("Press any button..");
             Console.ReadKey();
         }
