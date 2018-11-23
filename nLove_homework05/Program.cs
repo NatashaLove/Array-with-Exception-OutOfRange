dd15b97e2316807263991d21a51b121450d9bda9
Natasha S Love <debora@inbox.ru>
2018-11-22 19:59:39 -0600

    Update Program.cs

--- nLove_homework05/Program.cs
+++ nLove_homework05/Program.cs
@@ -6,7 +6,32 @@ namespace nLove_homework05
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string [] names = new string [5] { "Ann", "Betty", "Clair", "Donna", "Helen" };
+
+            Console.WriteLine(names[0]);
+            /*
+             * try
+                {
+                    Console.WriteLine("How many miles did you go?");
+                    t.Miles = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("How many gallons did you use?");
+                    t.Gallons = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("You got about {0} miles per gallon.", t.MPG);
+                }
+
+                catch (NegativeMilesException ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                } 
+             * 
+             */
+
+
+
+            Console.WriteLine("Press any button..");
+            Console.ReadKey();
         }
     }
 }
